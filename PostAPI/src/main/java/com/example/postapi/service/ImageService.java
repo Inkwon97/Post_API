6cd7100ab3d78679a048c7b05bd64148bbb9a2a0
01192mg <01192mg@gmail.com>
2022-08-06 09:15:10 +0900

    이미지 업로드 기능 추가

---
+++ PostAPI/src/main/java/com/example/postapi/service/ImageService.java
@@ -0,0 +1,60 @@
+package com.example.postapi.service;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Service
+@NoArgsConstructor
+public class ImageService {
+
+    private AmazonS3 s3Client;
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @PostConstruct
+    private void setS3Client() {
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+
+        s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(this.region)
+                .build();
+    }
+
+    public String upload(MultipartFile file) {
+        try (InputStream inputStream = file.getInputStream()) {
+            String fileName = file.getOriginalFilename();
+
+            s3Client.putObject(new PutObjectRequest(bucketName, fileName, inputStream, null)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+
+            return s3Client.getUrl(bucketName, fileName).toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
