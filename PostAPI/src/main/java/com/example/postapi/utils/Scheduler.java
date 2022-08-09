c665b93661c128ed1175f8b38c17ea66c12ce473
ghwo68 <ghwo68@naver.com>
2022-08-10 00:39:11 +0900

    스케줄러 기능 구현 수정완료

--- PostAPI/src/main/java/com/example/postapi/utils/Scheduler.java
+++ PostAPI/src/main/java/com/example/postapi/utils/Scheduler.java
@@ -2,29 +2,32 @@ package com.example.postapi.utils;
 
 import com.example.postapi.domain.Post;
 import com.example.postapi.repository.PostRepository;
-import com.example.postapi.service.PostService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class Scheduler {
-    private final PostService postService;
     private final PostRepository postRepository;
 
     // 초, 분, 시, 일, 월, 주
-    @Scheduled(cron = "0 */1 * * * *")
+    @Scheduled(cron = "0 0 1 * * *")
     public void autoDeletePost() throws InterruptedException {
         System.out.println("게시글 삭제 실행");
         // 저장된 모든 게시글 조회
         List<Post> postList = postRepository.findAll();
         for (Post post : postList){
+            TimeUnit.SECONDS.sleep(1);
             if (post.getComments().isEmpty()) {
-                Long id = post.getId();
-                postService.autoDeletePost(id);
+                postRepository.delete(post);
+
+                log.info("게시물 <{}>이 삭제되었습니다.", post.getTitle());
             }
         }
     }
