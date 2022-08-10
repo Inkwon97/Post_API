a2e2f661a896b8fd5f44ef72f1558cafeabc3d4f
ingwon97 <ghkdrnjs180@naver.com>
2022-08-10 14:45:29 +0900

    계정 이름 변경

--- PostAPI/src/main/java/com/example/postapi/repository/PostHeartRepository.java
+++ PostAPI/src/main/java/com/example/postapi/repository/PostHeartRepository.java
@@ -1,5 +1,6 @@
 package com.example.postapi.repository;
 
+import com.example.postapi.domain.Comment;
 import com.example.postapi.domain.Member;
 import com.example.postapi.domain.Post;
 import com.example.postapi.domain.PostHeart;
@@ -10,4 +11,5 @@ import java.util.Optional;
 public interface PostHeartRepository extends JpaRepository<PostHeart, Long> {
     Optional<PostHeart> findByMemberAndPost(Member member, Post post);
     int countByPost(Post post);
+
 }
